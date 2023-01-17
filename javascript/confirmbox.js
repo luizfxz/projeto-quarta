aecde21d273dd210c985e407be3c79be046df1c5
L.F~ <110194660+luizfxz@users.noreply.github.com>
2023-01-17 16:54:04 -0300

    att-final

---
+++ javascript/confirmbox.js
@@ -0,0 +1,5 @@
+function confirmBox() {
+
+    if (confirm("Você deseja mesmo ir para o Site do Google?")) { location.href="http://google.com";}
+
+}
---
+++ javascript/variaveis.js
@@ -0,0 +1,27 @@
+var pool = 2; // Número
+
+var nome = "Fulano"; // String
+
+let time;
+
+const doce = "chocolate"; // Este valor não pode ser alterado
+
+
+
+
+if (pool == 2) {
+
+    // executa este bloco
+
+} else if (pool == 3) {
+
+    // executa este bloco
+
+} else {
+
+    // executa este bloco
+
+}
+
+
+
